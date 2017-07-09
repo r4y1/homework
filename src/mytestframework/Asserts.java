52a2931e9222f8d2f8776bbc15e5b1493467fa5c
r4y1 <r4y1@protonmail.ch>
2017-07-09 17:00:41 +0300

    add assert class

---
+++ src/mytestframework/Asserts.java
@@ -0,0 +1,44 @@
+package mytestframework;
+
+public class Asserts {
+	public static void assertEquals(String actual, String expected) {
+		boolean equals = actual.equals(expected);
+		if (!equals) {
+			System.err.println("Test failed: expected '" + expected
+					+ "', but actual '" + actual + "'");
+		} else {
+			System.out.println("Test passed: expected '" + expected
+					+ "', actual '" + actual + "'");
+		}
+	}
+	public static void assertNotEquals(String actual, String expected) {
+		boolean notEquals = !actual.equals(expected);
+		if (!notEquals) {
+			System.err.println("Test failed: expected NOT '" + expected
+					+ "', but actual '" + actual + "'");
+		} else {
+			System.out.println("Test passed: expected NOT '" + expected
+					+ "', actual '" + actual + "'");
+		}
+	}
+	public static void assertEquals(int actual, int expected) {
+		boolean equals = actual == expected;
+		if (!equals) {
+			System.err.println("Test failed: expected '" + expected
+					+ "', but actual '" + actual + "'");
+		} else {
+			System.out.println("Test passed: expected '" + expected
+					+ "', actual '" + actual);
+		}	
+	}
+	public static void assertNotEquals(int actual, int expected) {
+		boolean notEquals = actual != expected;
+		if (!notEquals) {
+			System.err.println("Test failed: expected NOT '" + expected
+					+ "', but actual '" + actual + "'");
+		} else {
+			System.out.println("Test passed: expected NOT '" + expected
+					+ "', actual '" + actual + "'");
+		}
+	}
+}
