921bdcc889ce909ce4a613760c3f24e4a79246fc
r4y1 <r4y1@protonmail.ch>
2017-07-09 18:34:04 +0300

    update replace method

--- src/homework170707/StringMethods.java
+++ src/homework170707/StringMethods.java
@@ -3,9 +3,16 @@ package homework170707;
 public class StringMethods{
 
 	static String replace(String str, String target, String replacement) {
-		String result = "";
+		String result;
 		if (str.indexOf(target) != -1) {
-			
+			if (str.indexOf(target) == 0) {
+				result = replacement + str.substring(1);
+			}
+			result = str.substring(0, str.indexOf(target)) + 
+					replacement + 
+					str.substring(str.indexOf(target) + replacement.length());
+		} else {
+			result = str;
 		}
 		return result;
 	}
@@ -14,6 +21,10 @@ public class StringMethods{
 		String result = "";
 		
 		return result;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("test".indexOf("e"));
 	}
 	
 	
--- src/homework170707/StringMethodsTest.java
+++ src/homework170707/StringMethodsTest.java
@@ -6,6 +6,9 @@ public class StringMethodsTest {
 
 	public static void main(String[] args) {
 		Asserts.assertEquals(StringMethods.replace("", "e", "e"), "");
+		Asserts.assertEquals(StringMethods.replace("test", "e", "e"), "test");
+		Asserts.assertEquals(StringMethods.replace("hello", "e", "a"), "hallo");
+		Asserts.assertEquals(StringMethods.replace("test", "t", "r"), "rest");
 	}
 	
 	
