629716c69621857d4d09ec269d6f570d6dfd9af6
r4y1 <r4y1@protonmail.ch>
2017-07-09 17:00:15 +0300

    passed first test in replace method

---
+++ src/homework170707/StringMethods.java
@@ -0,0 +1,20 @@
+package homework170707;
+
+public class StringMethods{
+
+	static String replace(String str, String target, String replacement) {
+		String result = "";
+		if (str.indexOf(target) != -1) {
+			
+		}
+		return result;
+	}
+	
+	static String trim(String str) {
+		String result = "";
+		
+		return result;
+	}
+	
+	
+}
---
+++ src/homework170707/StringMethodsTest.java
@@ -0,0 +1,12 @@
+package homework170707;
+
+import mytestframework.*;
+
+public class StringMethodsTest {
+
+	public static void main(String[] args) {
+		Asserts.assertEquals(StringMethods.replace("", "e", "e"), "");
+	}
+	
+	
+}
