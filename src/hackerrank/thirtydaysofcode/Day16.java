e6e93ea56ce64365885e96354c28757d43f33917
r4y1 <r4y1@protonmail.ch>
2017-08-03 01:15:57 +0300

    solve day16 hackerrnank task

---
+++ src/hackerrank/thirtydaysofcode/Day16.java
@@ -0,0 +1,17 @@
+package hackerrank.thirtydaysofcode;
+
+import java.util.Scanner;
+
+public class Day16 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        try {
+            System.out.println(Integer.parseInt(S));
+        } catch (NumberFormatException e) {
+            System.out.println("Bad String");
+        } catch (NullPointerException e) {
+            System.out.println("Bad String");
+        }
+    }
+}
