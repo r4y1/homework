d407d55d6e6692d0ff19dffa67caf9e9256193be
r4y1 <r4y1@protonmail.ch>
2017-08-03 01:16:06 +0300

    solve day17 hackerrnank task

---
+++ src/hackerrank/thirtydaysofcode/Day17.java
@@ -0,0 +1,12 @@
+package hackerrank.thirtydaysofcode;
+
+public class Day17 {
+    static class Calculator {
+        int power(int n, int p) throws Exception {
+            if (n < 0 || p < 0) {
+                throw new Exception("n and p should be non-negative");
+            }
+            return (int) Math.pow(n, p);
+        }
+    }
+}
